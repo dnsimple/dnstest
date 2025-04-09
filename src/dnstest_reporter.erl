1007c9442a73bfc3658fa5f6c1ca0b6fb04670bd
Nelson Vides <nelson.vides@erlang-solutions.com>
2025-04-09 18:12:47 +0200

    Fix formatting for reporter

--- src/dnstest_reporter.erl
+++ src/dnstest_reporter.erl
@@ -6,6 +6,10 @@
 -spec report([dnstest_harness:return()]) -> ok.
 report(TestResults) ->
     {Pass, Fail} = lists:partition(fun(#{result := Result}) -> true =:= Result end, TestResults),
-    ?LOG_INFO(#{passed_num => length(Pass), passed => lists:map(fun(#{name := Name}) -> Name end, Pass),
-                failed_num => length(Fail), failed => lists:map(fun(#{name := Name}) -> Name end, Fail)}),
+    ?LOG_INFO(#{
+        passed_num => length(Pass),
+        passed => lists:map(fun(#{name := Name}) -> Name end, Pass),
+        failed_num => length(Fail),
+        failed => lists:map(fun(#{name := Name}) -> Name end, Fail)
+    }),
     lists:foreach(fun(Return) -> ?LOG_INFO(Return) end, Fail).
